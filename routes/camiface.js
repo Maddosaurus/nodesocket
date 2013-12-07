238a0bf7642a8056c62295820e4f23e173adef4a
Matthias <matthias@meidinger.de>
2013-12-07 20:29:25 +0100

    fixed ip-settings. now relative to caller, not hardcoded localhost

--- routes/camiface.js
+++ routes/camiface.js
@@ -2,11 +2,26 @@ var path 	= require('path'),
 	fs 		= require('fs'),
 	mime 	= require('mime');
 
+//look for IP of the host system
+var sysip;
+var os=require('os');
+var ifaces=os.networkInterfaces();
+for (var dev in ifaces) {
+	var alias=0;
+	ifaces[dev].forEach(function(details){
+		if (details.family=='IPv4' & details.internal==false) {
+			sysip = details.address;
+			++alias;
+		}
+	});
+}
+
 var mongo = require('mongodb');
 var Grid = require('gridfs-stream');
 var gfs;
 
-var db = new mongo.Db('grDB', new mongo.Server("192.168.1.119", 27017));
+//var db = new mongo.Db('grDB', new mongo.Server("192.168.1.119", 27017));
+var db = new mongo.Db('grDB', new mongo.Server(sysip, 27017));
 
 db.open(function(err) {
 	if(err) return handleError(err);
@@ -14,16 +29,17 @@ db.open(function(err) {
 });
 
 exports.getCamera = function(req,res) {
+
 	var id        = req.params[0];
-    var filename  = req.params[1];
+	var filename  = req.params[1];
 
 	res.set('Content-type', mime.lookup(filename));
-	//console.dir("AAA");
+	console.dir("AAA");
 
 	gfs
 	 .createReadStream({ _id: id })
 	 .on('error', function(err) {
-	 	res.send(404);
+		res.send(404);
 	 })
 	 .pipe(res);
 };
